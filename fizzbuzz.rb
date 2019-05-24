e41dbdf3d3b9a95625099aa410752951df004925
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-24 16:16:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-.upto(100) do |i|
-
+.upto(100) do 
   if i % 3 == 0 && i % 5 == 0 
     puts "FizzBuzz"
   elseif i % 3 == 0 
