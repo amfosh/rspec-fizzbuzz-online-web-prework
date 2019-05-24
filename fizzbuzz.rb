2fe6a9e3b57281a0c8a71619257e4ae4af4fe645
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-24 16:15:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int=1..100)
+  if int % 3 == 0 && int % 5 == 0 
+    puts "FizzBuzz"
+  elseif int % 3 == 0 
+    puts "Fizz"
+  elseif int % 5 == 0 
+    puts "Buzz"
+  end
+end
