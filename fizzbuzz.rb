e9f871057c2fa82743a9ad7b72e1e1afe122158a
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-24 16:15:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 .upto(100) do |i|
-  
-  def fizzbuzz(int=1..100)
-  if int % 3 == 0 && int % 5 == 0 
+
+  if i % 3 == 0 && i % 5 == 0 
     puts "FizzBuzz"
-  elseif int % 3 == 0 
+  elseif i % 3 == 0 
     puts "Fizz"
-  elseif int % 5 == 0 
+  elseif i % 5 == 0 
     puts "Buzz"
   end
 end
